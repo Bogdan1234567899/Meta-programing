af0475171431f8c789ba4d1bc3e172180d2c5c3e
Bogdan1234567899 <81978094+Bogdan1234567899@users.noreply.github.com>
2025-10-29 12:26:26 +0200

    Create Program.cs

---
+++ Lesson 5/Program.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace CollectionsHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+            Task1_ForEach.Run();
+            Console.WriteLine("\n\n");
+            Pause();
+
+            
+            Task2_Book.Run();
+            Console.WriteLine("\n\n");
+            Pause();
+
+            
+            Task3_BookComparers.Run();
+            Console.WriteLine("\n\n");
+            
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("Натисніть будь-яку клавішу для продовження...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
